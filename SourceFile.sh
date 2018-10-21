1a5257d9d9824c7d77231c602e0f48ed46dd2112
Georg Großmann <georg.grossmann@fau.de>
2018-10-21 23:42:48 +0200

    changed wording

--- SourceFile.sh
+++ SourceFile.sh
@@ -1,6 +1,9 @@
 if [ ! -f "$1" ] || [ "${UseLocalFiles}" -eq 0 ]; then
-	echo "We will now get the file: ${Github_Repository}/${Github_Branch}/$1 and save it to ${Local_Repository}/$1"
+	echo "We will now get the file:"
+	echo "${Github_Repository}/${Github_Branch}/$1"
+	echo "The file will be saved as:"
+	echo "${Local_Repository}/$1"
 	curl -sL "${Github_Repository}/${Github_Branch}/$1" --create-dirs -o "${Local_Repository}/$1"
 fi
-echo "We will now source the file: ${Local_Repository}/$1"
+echo "We following file will be sourced now: ${Local_Repository}/$1"
 source "${Local_Repository}/$1"
