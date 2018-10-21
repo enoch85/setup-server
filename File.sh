a85f53384a5bda831a97479151421ce264a86cfd
Georg Großmann <georg.grossmann@fau.de>
2018-10-21 21:23:42 +0200

    Switch back to curl because of possibility to create subdirectories

--- File.sh
+++ File.sh
@@ -1,6 +1,5 @@
 if [ ! -f "$1" ] || [ "${UseLocalFiles}" -eq 0 ]
 then
-	# source <(curl -sL "${Github_Repository}/${Github_Branch}/lib.sh")
-	wget -O $1 "${Github_Repository}/${Github_Branch}/$1"
+	curl -sL "${Github_Repository}/${Github_Branch}/$1" --create-dirs -o $1
 fi
 source "$1"
